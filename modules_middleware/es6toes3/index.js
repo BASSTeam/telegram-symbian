5644a8146b6a1d9cb5947313d7570492ca3504af
KaMeHb-UA <marlock@etlgr.com>
2018-09-13 15:45:32 +0300

    UglifyJS updated to UglifyES, prepare to rename middleware module, package descriptor for middleware module

--- modules_middleware/es6toes3/index.js
+++ modules_middleware/es6toes3/index.js
@@ -1,18 +1,32 @@
 const {
         readFileSync: read,
         writeFileSync: write,
-        unlinkSync: rm,
     } = require('fs'), {
         spawnSync: spawn,
-    } = require('child_process');
+    } = require('child_process'), {
+        minify,
+    } = require('uglify-es');
 
-module.exports = input => {
+class ES6_Code extends String{};
+class ES3_Code extends String{};
+/**
+ * @param {ES6_Code} input
+ * @return {ES3_Code}
+ */
+exports.compile = input => {
     write(__dirname + '/temp/input/script.js', input, 'utf8');
     spawn(__dirname + '/node_modules/.bin/tsc', {
         cwd: __dirname
     });
     var _ = read(__dirname + '/temp/output/script.js', 'utf8');
-    rm(__dirname + '/temp/input/script.js');
-    rm(__dirname + '/temp/output/script.js');
     return _
+}
+/**
+ * @param {ES6_Code} input
+ * @return {ES6_Code}
+ */
+exports.minify = input => {
+    const result = minify(input);
+    if(result.error) throw result.error;
+    return result.code
 }
