814fdf7234a6c70ad437b005ad8a2aed2163d6f6
KaMeHb-UA <marlock@etlgr.com>
2018-09-13 16:31:07 +0300

    Compiling is not needed at build stage now

--- src/js/lib/require.next/build.js
+++ src/js/lib/require.next/build.js
@@ -1,8 +1,5 @@
-const compile = require('modules_middleware/es6toes3'),
-    { readFileSync: read } = require('fs');
+const { readFileSync: read } = require('fs');
 
-module.exports = compile(
-    read(__dirname + '/main.js', 'utf8')
-        .split('/**\n * @from Promise_implementation.js\n */')
-        .join(read(__dirname + '/Promise_implementation.js', 'utf8'))
-);
+module.exports = () => read(__dirname + '/main.js', 'utf8')
+    .split('/**\n * @from Promise_implementation.js\n */')
+    .join(read(__dirname + '/Promise_implementation.js', 'utf8'))
