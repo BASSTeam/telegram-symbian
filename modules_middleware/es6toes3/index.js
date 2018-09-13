afdf6ee756aacc7129e308a9348552cc5fc8a3a2
KaMeHb-UA <marlock@etlgr.com>
2018-09-13 12:59:19 +0300

    Added ES6-to-ES3 conterter as a middleware

---
+++ modules_middleware/es6toes3/index.js
@@ -0,0 +1,18 @@
+const {
+        readFileSync: read,
+        writeFileSync: write,
+        unlinkSync: rm,
+    } = require('fs'), {
+        spawnSync: spawn,
+    } = require('child_process');
+
+module.exports = input => {
+    write(__dirname + '/temp/input/script.js', input, 'utf8');
+    spawn('tsc', {
+        cwd: __dirname
+    });
+    var _ = read(__dirname + '/temp/output/script.js', 'utf8');
+    rm(__dirname + '/temp/input/script.js');
+    rm(__dirname + '/temp/output/script.js');
+    return _
+}
