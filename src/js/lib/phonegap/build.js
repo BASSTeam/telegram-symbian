a060011c01ff7968fa0a24a9baf34611fbe2c5dd
KaMeHb-UA <marlock@etlgr.com>
2018-09-15 12:44:29 +0300

    Build system update

--- src/js/lib/phonegap/build.js
+++ src/js/lib/phonegap/build.js
@@ -1,1 +1,20 @@
-module.exports = () => ''
+const {
+        readFileSync: read,
+        readdirSync: list,
+    } = require('fs'),
+    partsDir = __dirname + '/parts';
+
+module.exports = () => {
+    var res = read(`${partsDir}/phonegap.js.base`, 'utf8');
+    list(partsDir).forEach(part => {
+        if(!/\.js$/.test(part)) return;
+        res += read(`${partsDir}/${part}`, 'utf8');
+        if(part == 'camera.js') [
+            'com.nokia.device.utility',
+            'com.nokia.device.framework',
+            's60_camera',
+            'com.nokia.device.camera',
+        ].forEach(name => res += read(`${partsDir}/camera/${name}.js`, 'utf8'));
+    });
+    return res
+}
